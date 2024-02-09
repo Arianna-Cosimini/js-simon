881fc55f60e4337fce21a4faa70bf5c893351dcb
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-10 00:05:43 +0100

    iniziato stile

--- js/script.js
+++ js/script.js
@@ -11,32 +11,63 @@ const countDownStart = setInterval(function () {
     console.log(nowDate)
 
     // Calcolo la differenza di ms tra countDown e data di ora
-    const differenceDate = countDownDate - nowDate;
-    console.log(differenceDate)
+    
+    let differenceDate = countDownDate - nowDate;
+    document.querySelector(".totale-millisecondi").innerHTML = differenceDate;
+    console.log(differenceDate);
+
+
+
+
 
 
 
     // Calcolo delle dei giorni rimanenti
-    const days = Math.floor(differenceDate / (1000 * 60 * 60 * 24));
-    
+    let days = Math.floor(differenceDate / (1000 * 60 * 60 * 24));
+
     // Calcolo delle delle ore rimanenti
-    const hours = Math.floor((differenceDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    
+    let hours = Math.floor((differenceDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
     // Calcolo delle dei minuti rimanenti
-    const minutes = Math.floor((differenceDate % (1000 * 60 * 60)) / (1000 * 60));
-    
-    
+    let minutes =Math.floor((differenceDate % (1000 * 60 * 60)) / (1000 * 60));
+
+
     // Calcolo delle dei secondi rimanenti
-    const seconds = Math.floor((differenceDate % (1000 * 60)) / 1000);
+    let seconds =Math.floor((differenceDate % (1000 * 60)) / 1000);
+
+
+
     
-    // Calcolo delle dei millisecondi rimanenti
-    const milliSeconds = Math.floor(differenceDate % (1000 * 1000) / 1000);
-        console.log(days, hours + "h", minutes + "m", seconds + "s", milliSeconds + "ms");
+    // / Calcolo delle dei millisecondi rimanenti
+    let milliSeconds = Math.floor (differenceDate % 1000);
+    milliSeconds = document.querySelector(".milliseconds").innerHTML = milliSeconds;
+
+    console.log( milliSeconds + "ms");
+
+
+
+    days = document.querySelector(".days").innerHTML =days;
+    hours = document.querySelector(".hours").innerHTML = hours;
+    minutes = document.querySelector(".minutes").innerHTML = minutes;
+    seconds = document.querySelector(".seconds").innerHTML = seconds;
 
 
     // blocco la funzione setInterval se il conteggio arriva a 0
     if (differenceDate < 0) {
         clearInterval(countDownStart);
+        document.querySelector(".milliseconds").innerHTML = 0;
+        document.querySelector(".days").innerHTML = 0;
+        document.querySelector(".hours").innerHTML = 0;
+        document.querySelector(".minutes").innerHTML = 0;
+        document.querySelector(".seconds").innerHTML = 0;
+        document.querySelector(".totale-millisecondi").innerHTML = 0;
+        console.log("Il tempo è terminato");
+        return;
+
     }
-}, 1000);
+}, 0);
+
+
+
+
 
