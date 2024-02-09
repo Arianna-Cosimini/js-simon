6d9b3d149c6661d7cd00ba2d9270c2a43ccfc49b
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-09 14:59:23 +0100

    Iniziato inserendo new date per data in cui termina il count down e inserito data di oggi.Entrambe convertite in millisecondi

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+// Inserisco data e ora da raggiungere con il countDown e converto in ms
+let countDownDate = new Date("Feb 12, 2024 9:30:00").getTime();
+console.log(countDownDate)
+
+// Inserisco data e ora attuale e converto in ms
+let nowDate= new Date().getTime();
+console.log(nowDate)
+
+// Calcolo la differenza di ms tra countDown e data di ora
+const differenceDate = countDownDate - nowDate;
+console.log(differenceDate)
