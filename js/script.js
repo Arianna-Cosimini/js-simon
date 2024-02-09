2900262877ef031a93e08d19a9d7c65aeac34841
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-09 15:34:35 +0100

    inserita timing function setInterval

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-
-
+//  iserisco tutto in una funzione setInterval 
+const countDownStart = setInterval (function(){
 // Inserisco data e ora da raggiungere con il countDown e converto in ms
 let countDownDate = new Date("Feb 12, 2024 9:30:00").getTime();
 console.log(countDownDate)
@@ -12,3 +12,5 @@ console.log(nowDate)
 // Calcolo la differenza di ms tra countDown e data di ora
 const differenceDate = countDownDate - nowDate;
 console.log(differenceDate)
+},1000);
+
